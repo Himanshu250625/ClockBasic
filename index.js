24c222faded17ac2e00efcf5f74937b534df6098
Himanshu Singh <db3131444@gmail.com>
2025-04-18 15:03:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 
-function setDate() {
+function setClock() {
   const now = new Date();
-
   const getSecond = now.getSeconds();
   const getMinute = now.getMinutes();
   const getHour = now.getHours();
 
-  const secondDegree = (getSecond / 60) * 360;
-  const minuteDegree = (getMinute / 60) * 360;
-  const hourDegree = (getHour / 12) * 360;
+  const secondDeg = (getSecond / 60) * 360;
+  const minuteDeg = ((getMinute + getSecond / 60) / 60) * 360;
+  const hourDeg = ((getHour % 12 + getMinute / 60) / 12) * 360;
 
-  second.style.transform = `rotate(${secondDegree}deg)`;
-  minute.style.transform = `rotate(${minuteDegree}deg)`;
-  hour.style.transform = `rotate(${hourDegree}deg)`;
+  second.style.transform = `rotate(${secondDeg}deg)`;
+  minute.style.transform = `rotate(${minuteDeg}deg)`;
+  hour.style.transform = `rotate(${hourDeg}deg)`;
 }
 
-setInterval(setDate, 1000);
+setInterval(setClock, 1000);
+setClock();
